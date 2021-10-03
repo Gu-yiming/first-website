3897253aea3f84d9af50156ee5cebf82f0d54e6a
Gu-yiming <90350347+Gu-yiming@users.noreply.github.com>
2021-10-03 14:29:30 -0400

    2
    2

---
+++ first website.bat
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!- -[if It IE 7]]>   <html class= "no-js It-ie9 It-ie8 It-ie 7"> <![endif]- - >
+<!- -[if IE 7]]>   <html class= "no-js It-ie9 It-ie8"> <![endif]- - >
+<!- -[if Ie 8]]>.  <html class= "no-js It-ie9"> <![endif]- - >
+<!- -[if gt IE 8]]>   <html class="no-js" < ! - - <![endif]- - >
+<html lang="fr">
+     <head>
+         <meta charset = "utf-8">
+         <meta http-equiv-"X-UA-Compatible" content="IE-edge">
+         <title></title>
+         <meta name-"description" content ="">
+         <meta name-"viewport" content-"width-devith-width, initiale-1">
+         link rel-"stlyesheet" href-"">
+     </head>
+     <body>
+   <h1>Mon titre prise 2</hI>
+   <ing src-"mer-bleu.png" alt-"Le Marais de la Mer Bleue de l'automne." width-568"> 
+   <p>Ceci est un paragraphe.</p>
+   
+   <iframe width 268 height-"315" src-"https://www.youtube.com>
+<br>
+   <ing src-"boardwalk.jpg" alt-"La passerrelle de bois de la Mer Bleue." width-568"> 
+   <blockquote class= "twitter-tweet"><p lang-"fr" dir="Itr">Petite victoire
+</body>
+</html>
